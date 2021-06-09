8a2bec7c21d1ae82cc08d5c7bbdd554e1e1d28ea
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-09 22:18:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let Name = 'Eline'; //string literal
+console.log("Hallo Winc Academy studenten");
+
+document.write(1230941 x 1823794)
+
+document.write(637263 / 54)
+
+
+let interestRate = 0,3;
+interestRate = 1;
+console.log(interestRate);
+
+// Verander deze input om te zien of je code werkt.
+// Comment bijvoorbeeld regel 8 tot 11 weer aan, en regel 3 tot 5 uit.
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+/*
+Testdata
+const age = 16; 
+const isFemale = false;
+const driverStatus = 808; 
+*/
+
+if (age >= 18) {
+    console.log("Je mag naar binnen");
+} else {
+    console.log("Nope, je mag niet naar binnen");
+}
+
+if (isFemale) {
+    console.log("Je ticket zegt dat je een vrouw bent!");
+} else {
+    console.log(
+        "Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen"
+    );
+}
+
+if (driverStatus === "bob") {
+    console.log("Je bent de bob, je mag autorijden");
+} else {
+    console.log("You can better get a cab!");
+}
+
+
+
+
+
+if (age >= 17 && age <= 26) {
+    document.write("Je krijgt 50% korting!");
+}
+
+/*tussen de 18 en 25 jaar een korting van 50% op hun biertjes krijgen.*/
+
+
+if (Name Sarah || Name Bram) {
+    document.write("Gratis bier voor jou!");
+
+}
+
+/*Sarah" of "Bram*/
+
+
+if (euro >= 25) {
+    document.write("Gratis bitterballen voor jou!");
+
+
+}
+
+if (euro >= 50) {
+    document.write("Gratis nacho's voor jou!");
+}
+/*Bestel je voor meer dan 25 euro aan drankjes? Dan krijg je gratis (vega)bitterballen.
+Bestel je voor meer dan 50 euro aan drankjes? Dan krijg je gratis portie nachos.*/
